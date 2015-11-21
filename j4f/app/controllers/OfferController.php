a009b119da4889388a03415a197cd2489570cb1e
Giap Nguyen <nvangiap5894@gmail.com>
2015-11-21 16:49:52 +0700

    update status offer

--- j4f/app/controllers/OfferController.php
+++ j4f/app/controllers/OfferController.php
@@ -133,5 +133,46 @@ class OfferController extends \Phalcon\Mvc\Controller {
 		return $response;
 	}
 
+	public function updatestatusAction() {
+		$response = new ApiResponse();
+
+		if ( $this->request->isPost() ) {
+			$users_id  = $this->request->getPost( 'users_id' );
+			$offers_id = $this->request->getPost( 'offers_id' );
+
+			if ( $offers_id != null && $users_id != null ) {
+
+				$offer = Offers::findFirstById( $offers_id );
+
+				if ( $offer == true ) {
+					$offer->status          = 1; // accepted
+					$offer->offered_user_id = $users_id;
+
+					try {
+						if ( $offer->save() == false ) {
+							$response->setResponseError( implode( ', ', $offer->getMessages() ) );
+						} else {
+							$response->setResponseMessage( "User $users_id has been accepted for offer $offers_id!" );
+						}
+					} catch ( PDOException $e ) {
+						$response->setResponseError( $e->getMessage() );
+					}
+				} else {
+					$response->setResponseError( 'Not found offer!' );
+
+					return $response;
+				}
+			} else {
+				$response->setResponseError( 'Insufficient parameters' );
+
+				return $response;
+			}
+		} else {
+			$response->setResponseError( 'Wrong HTTP Method' );
+		}
+
+		return $response;
+	}
+
 }
 
