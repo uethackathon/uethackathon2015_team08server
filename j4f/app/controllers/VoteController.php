4fcefc33799d3522c57e76c0d4ac2505307ca93b
Giap Nguyen <nvangiap5894@gmail.com>
2015-11-21 14:51:50 +0700

    add vote up/down

--- j4f/app/controllers/VoteController.php
+++ j4f/app/controllers/VoteController.php
@@ -1,12 +1,184 @@
 <?php
 
-class VoteController extends \Phalcon\Mvc\Controller
-{
+class VoteController extends \Phalcon\Mvc\Controller {
 
-    public function indexAction()
-    {
+	public function indexAction() {
+	}
 
-    }
+	public function upAction() {
+		$response = new ApiResponse();
+		$value    = 1;
+
+		if ( $this->request->isGet() ) {
+			$questions_id = $this->request->get( 'questions_id' );
+			$answers_id   = $this->request->get( 'answers_id' );
+			$users_id     = $this->request->get( 'users_id' );
+
+			if ( $questions_id != null && $answers_id == null ) {
+				$question = Questions::findFirstById( $questions_id );
+				if ( $question == false ) {
+					$response->setResponseError( 'Question not found!' );
+
+					return $response;
+				} else {
+					if ( $question->users_id != $users_id
+					     && ! Votes::checkIfVotedQuestion( $users_id, $questions_id )
+					) {
+						$question->upvotes += 1;
+						if ( $question->save() == false ) {
+							$response->setResponseError( $question->getMessages() );
+
+							return $response;
+						}
+
+						$vote               = new Votes();
+						$vote->id           = uniqid();
+						$vote->questions_id = $questions_id;
+						$vote->users_id     = $users_id;
+						$vote->value        = $value;
+						if ( $vote->save() == false ) {
+							$response->setResponseError( $vote->getMessages() );
+
+							return $response;
+						} else {
+							$response->setResponseMessage( "Create vote up $vote->id for question $questions_id successfully!" );
+						}
+					} else {
+						$response->setResponseError( 'Cant re-vote or vote for yourself!' );
+
+						return $response;
+					}
+				}
+			} else if ( $questions_id == null && $answers_id != null ) {
+				$answer = Answers::findFirstById( $answers_id );
+				if ( $answer == false ) {
+					$response->setResponseError( 'Answer not found!' );
+
+					return $response;
+				} else {
+					if ( $answer->users_id != $users_id
+					     && ! Votes::checkIfVotedAnswer( $users_id, $answers_id )
+					) {
+						$answer->upvotes += 1;
+						if ( $answer->save() == false ) {
+							$response->setResponseError( $answer->getMessages() );
+
+							return $response;
+						}
+
+						$vote             = new Votes();
+						$vote->id         = uniqid();
+						$vote->answers_id = $answers_id;
+						$vote->users_id   = $users_id;
+						$vote->value      = $value;
+						if ( $vote->save() == false ) {
+							$response->setResponseError( $vote->getMessages() );
+
+							return $response;
+						} else {
+							$response->setResponseMessage( "Create vote up $vote->id for answer $answers_id successfully!" );
+						}
+					} else {
+						$response->setResponseError( 'Cant re-vote or vote for yourself!' );
+
+						return $response;
+					}
+				}
+			}
+		} else {
+			$response->setResponseError( 'Wrong HTTP Method' );
+		}
+
+		return $response;
+	}
+
+	public function downAction() {
+		$response = new ApiResponse();
+		$value    = - 1;
+
+		if ( $this->request->isGet() ) {
+			$questions_id = $this->request->get( 'questions_id' );
+			$answers_id   = $this->request->get( 'answers_id' );
+			$users_id     = $this->request->get( 'users_id' );
+
+			if ( $questions_id != null && $answers_id == null ) {
+				$question = Questions::findFirstById( $questions_id );
+				if ( $question == false ) {
+					$response->setResponseError( 'Question not found!' );
+
+					return $response;
+				} else {
+					if ( $question->users_id != $users_id
+					     && ! Votes::checkIfVotedQuestion( $users_id, $questions_id )
+					) {
+						$question->downvotes += 1;
+						if ( $question->save() == false ) {
+							$response->setResponseError( $question->getMessages() );
+
+							return $response;
+						}
+
+						$vote               = new Votes();
+						$vote->id           = uniqid();
+						$vote->questions_id = $questions_id;
+						$vote->users_id     = $users_id;
+						$vote->value        = $value;
+						if ( $vote->save() == false ) {
+							$response->setResponseError( $vote->getMessages() );
+
+							return $response;
+						} else {
+							$response->setResponseMessage( "Create vote down $vote->id for question $questions_id successfully!" );
+						}
+					} else {
+						$response->setResponseError( 'Cant re-vote or vote for yourself!' );
+
+						return $response;
+					}
+				}
+			} else if ( $questions_id == null && $answers_id != null ) {
+				$answer = Answers::findFirstById( $answers_id );
+				if ( $answer == false ) {
+					$response->setResponseError( 'Answer not found!' );
+
+					return $response;
+				} else {
+					if ( $answer->users_id != $users_id
+					     && ! Votes::checkIfVotedAnswer( $users_id, $answers_id )
+					) {
+						var_dump(Votes::checkIfVotedAnswer( $users_id, $answers_id ));
+						$answer->downvotes += 1;
+						if ( $answer->save() == false ) {
+							$response->setResponseError( $answer->getMessages() );
+
+							return $response;
+						}
+
+						$vote             = new Votes();
+						$vote->id         = uniqid();
+						$vote->answers_id = $answers_id;
+						$vote->users_id   = $users_id;
+						$vote->value      = $value;
+						if ( $vote->save() == false ) {
+							$response->setResponseError( $vote->getMessages() );
+
+							return $response;
+						} else {
+							$response->setResponseMessage( "Create vote down $vote->id for answer $answers_id successfully!" );
+						}
+					} else {
+						$response->setResponseError( 'Cant re-vote or vote for yourself!' );
+
+						return $response;
+					}
+				}
+			}
+		} else {
+			$response->setResponseError( 'Wrong HTTP Method' );
+		}
+
+		return $response;
+	}
 
 }
 
--- j4f/app/models/Votes.php
+++ j4f/app/models/Votes.php
@@ -1,71 +1,88 @@
 <?php
 
-class Votes extends \Phalcon\Mvc\Model
-{
+class Votes extends \Phalcon\Mvc\Model {
 
-    /**
-     *
-     * @var string
-     */
-    public $id;
+	/**
+	 *
+	 * @var string
+	 */
+	public $id;
 
-    /**
-     *
-     * @var integer
-     */
-    public $value;
+	/**
+	 *
+	 * @var interger
+	 */
+	public $value;
 
-    /**
-     *
-     * @var string
-     */
-    public $questions_id;
+	/**
+	 *
+	 * @var string
+	 */
+	public $questions_id;
 
-    /**
-     *
-     * @var string
-     */
-    public $answers_id;
+	/**
+	 *
+	 * @var string
+	 */
+	public $answers_id;
 
-    /**
-     * Initialize method for model.
-     */
-    public function initialize()
-    {
-        $this->belongsTo('answers_id', 'Answers', 'id', array('alias' => 'Answers'));
-        $this->belongsTo('questions_id', 'Questions', 'id', array('alias' => 'Questions'));
-    }
+	/**
+	 *
+	 * @var string
+	 */
+	public $users_id;
 
-    /**
-     * Returns table name mapped in the model.
-     *
-     * @return string
-     */
-    public function getSource()
-    {
-        return 'votes';
-    }
+	/**
+	 * Initialize method for model.
+	 */
+	public function initialize() {
+		$this->belongsTo( 'answers_id', 'Answers', 'id', array( 'alias' => 'Answers' ) );
+		$this->belongsTo( 'questions_id', 'Questions', 'id', array( 'alias' => 'Questions' ) );
+		$this->belongsTo( 'users_id', 'Users', 'id', array( 'alias' => 'Users' ) );
+	}
 
-    /**
-     * Allows to query a set of records that match the specified conditions
-     *
-     * @param mixed $parameters
-     * @return Votes[]
-     */
-    public static function find($parameters = null)
-    {
-        return parent::find($parameters);
-    }
+	/**
+	 * Returns table name mapped in the model.
+	 *
+	 * @return string
+	 */
+	public function getSource() {
+		return 'votes';
+	}
 
-    /**
-     * Allows to query the first record that match the specified conditions
-     *
-     * @param mixed $parameters
-     * @return Votes
-     */
-    public static function findFirst($parameters = null)
-    {
-        return parent::findFirst($parameters);
-    }
+	/**
+	 * Allows to query a set of records that match the specified conditions
+	 *
+	 * @param mixed $parameters
+	 *
+	 * @return Votes[]
+	 */
+	public static function find( $parameters = null ) {
+		return parent::find( $parameters );
+	}
+
+	/**
+	 * Allows to query the first record that match the specified conditions
+	 *
+	 * @param mixed $parameters
+	 *
+	 * @return Votes
+	 */
+	public static function findFirst( $parameters = null ) {
+		return parent::findFirst( $parameters );
+	}
+
+	public static function checkIfVotedQuestion( $users_id, $questions_id ) {
+		$vote = Votes::findFirst( "users_id = '$users_id' AND questions_id = '$questions_id'" );
+
+		return ( $vote != false );
+	}
+
+
+	public static function checkIfVotedAnswer( $users_id, $answer_id ) {
+		$vote = Votes::findFirst( "users_id = '$users_id' AND answers_id = '$answer_id'" );
+
+		return ( $vote != false );
+	}
 
 }
