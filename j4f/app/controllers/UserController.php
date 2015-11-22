c7c260e2972056b8662d86ca326a4997ebb69c89
Giap Nguyen <nvangiap5894@gmail.com>
2015-11-22 08:50:55 +0700

    change response

--- j4f/app/controllers/UserController.php
+++ j4f/app/controllers/UserController.php
@@ -26,6 +26,7 @@ class UserController extends \Phalcon\Mvc\Controller {
 			$user->username = $username;
 			$user->email    = $email;
 			$user->password = $password;
+//			$user->avatar =
 
 			// Store the password hashed
 			$user->password = $this->security->hash( $password );
@@ -33,7 +34,12 @@ class UserController extends \Phalcon\Mvc\Controller {
 				if ( $user->save() == false ) {
 					$response->setResponseError( implode( ', ', $user->getMessages() ) );
 				} else {
-					$response->setResponseMessage( $user->id );
+					$response->setResponse( array(
+						'id'       => $user->id,
+						'username' => $user->username,
+						'email'    => $user->email,
+						'avatar'   => $user->avatar
+					) );
 				}
 			} catch ( PDOException $e ) {
 				$response->setResponseError( $e->getMessage() );
@@ -67,7 +73,12 @@ class UserController extends \Phalcon\Mvc\Controller {
 				return $response;
 			}
 
-			$response->setResponseMessage( 'Login successfully!' );
+			$response->setResponse( array(
+				'id'       => $user->id,
+				'username' => $user->username,
+				'email'    => $user->email,
+				'avatar'   => $user->avatar
+			) );
 		} else {
 			$response->setResponseError( 'Wrong HTTP Method' );
 		}
